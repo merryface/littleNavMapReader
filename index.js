fbdf4008547cc815c8c885df55fca575f9ffc770
Tarik <tarikmerrylees@gmail.com>
2022-01-20 15:22:26 +0000

    quick documenation of JS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Function to change coordinates into degrees and minutes
 const toDegreesMinutesAndSeconds = coordinate => {
   const absolute = Math.abs(coordinate);
   const degrees = Math.floor(absolute);
@@ -9,6 +10,7 @@ const toDegreesMinutesAndSeconds = coordinate => {
   return `${degrees}${minutes}`;
 }
 
+// Degrees to convert decimal coordinates into DMS coordinates
 const convertDMS = (lat, lng) => {
   let latitude = toDegreesMinutesAndSeconds(lat);
   const latitudeCardinal = lat >= 0 ? "N" : "S";
@@ -33,6 +35,7 @@ const convertDMS = (lat, lng) => {
   return `${latitude}${latitudeCardinal}${longitude}${longitudeCardinal}`;
 }
 
+ // User selects file from disc
   document.getElementById('inputfile')
       .addEventListener('change', function() {
       const fr=new FileReader();
@@ -42,12 +45,14 @@ const convertDMS = (lat, lng) => {
         let airports = [];
         let routeInfo = [];
 
+        // Add all airport ICAOs into array
         planData.forEach(line => {
           if (line.substr(9)[0] == "I") {
             const airportElement = line.match("<Ident>(.*?)</Ident>");
             airports.push(airportElement[1]);
           }
 
+          // Add all coordinates into array
           if (line.substr(9)[0] == "P") {
             const long = line.match(`Lon="(.*?)"`)[1];
             const lat = line.match(`Lat="(.*?)"`)[1];
@@ -55,6 +60,8 @@ const convertDMS = (lat, lng) => {
             routeInfo.push(convertDMS(lat, long))
           }
         })
+
+        // create strings to be output
         const summary = airports.join(" ");
         const info = routeInfo.join(" \n ");
 
